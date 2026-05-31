1f48140048091f92bd68f3352bb9c7cd43d422be
chasesingleton2k22-beep <chasesingleton2k22@gmail.com>
2026-05-31 18:01:36 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bwr-personal-v31';
+const CACHE = 'bwr-personal-v47';
 const ASSETS = ['./', './index.html', './cover.svg', './manifest.json'];
 
 self.addEventListener('install', e => {
